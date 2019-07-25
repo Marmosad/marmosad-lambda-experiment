630792c6612ed60be20285822d8bfb0555f61e3d
Ern <Fw>
2019-07-25 12:55:27 -0700

    Nani

--- functions/onEvent/utils.js
+++ functions/onEvent/utils.js
@@ -2,10 +2,12 @@ module.exports = function pickJudge(players, currentJudge) {
     let playerIterator = Object.keys(players);
     let nextJudge;
     let i = 0;
-    while (currentJudge !== players[playerIterator[i]].connectionId && i < playerIterator.length) {
+    while (currentJudge !== playerIterator[i] && i < playerIterator.length) {
+        console.log(playerIterator[i], currentJudge);
         i++;
     }
 
+    console.log("the next judge index is", i);
     if (i === playerIterator.length)
         i = 0;
     else
